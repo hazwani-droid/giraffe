dd004a18ede6b1b48b8de668b3efcf6566fd3af4
hazwani-droid <hazw200@gmail.com>
2022-06-08 00:54:59 +0800

    update

--- src/views/Update.vue
+++ src/views/Update.vue
@@ -13,7 +13,7 @@
 
         <v-card-text>
           <v-container>
-            <v-form method="post" action="/company/add">
+            <v-form id="formid">
               <v-text-field
                 label="Company Name"
                 required
@@ -25,41 +25,41 @@
                 label="Ticker Label"
                 color="blue accent-3"
                 v-model="ticker"
-                  required
+                required
               ></v-text-field>
               <v-text-field
                 label="No. of Employee"
                 color="blue accent-3"
                 v-model="employee"
-                  required
+                required
               ></v-text-field>
 
               <v-text-field
                 label="Stock Price"
                 color="blue accent-3"
                 v-model="stockPrice"
-                  required
+                required
               ></v-text-field>
 
               <v-text-field
                 label="Market Capitalization"
                 color="blue accent-3"
                 v-model="marketCap"
-                  required
+                required
               ></v-text-field>
 
               <v-text-field
                 label="Directors"
                 color="blue accent-3"
                 v-model="director"
-                  required
+                required
               ></v-text-field>
 
               <v-text-field
                 label="Sector"
                 v-model="sector"
                 color="blue accent-3"
-                  required
+                required
               ></v-text-field>
 
               <v-text-field
@@ -67,72 +67,16 @@
                 dense
                 color="blue accent-3"
                 v-model="industry"
-                  required
+                required
               ></v-text-field>
               <v-text-field
                 label="Price per Earning Ratio"
                 color="blue accent-3"
                 v-model="peratio"
-                  required
+                required
               ></v-text-field>
-
-              <v-textarea
-                label="Cypher Query to be executed"
-                dense
-              ></v-textarea>
-              <small
-                >*Please refer<v-btn
-                  @click="dialog2 = !dialog2"
-                  plain
-                  x-small
-                  color="primary"
-                  class="text-none"
-                  >this</v-btn
-                >for Cypher queries guideline</small
-              >
             </v-form>
 
-            <v-dialog v-model="dialog2" max-width="500px">
-              <v-card>
-                <v-row>
-                  <v-spacer></v-spacer>
-                  <v-card-title class="justify-center">
-                    Cypher Query Guideline
-                  </v-card-title>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    plain
-                    fab
-                    @click="dialog2 = false"
-                    text
-                    color="primary"
-                  >
-                    <v-icon dark> mdi-close </v-icon>
-                  </v-btn>
-                </v-row>
-                <v-card-text
-                  >1. Please specify a label name for each node starting with a
-                  capital letter.</v-card-text
-                >
-                <v-card-text
-                  >2. Each node property name has to be short and simple without
-                  spacing.</v-card-text
-                >
-                <v-card-text
-                  >3. Example below shows the query of retrieving a company's
-                  node director's property with the specified name.</v-card-text
-                >
-                <v-card-text
-                  >e.g MATCH p=(Company {director: 'Siew Wei
-                  Mak'})-[r:DIRECTED_BY]->() RETURN p .</v-card-text
-                >
-
-                <v-card-text
-                  >4. Always refer to the Neo4j documentation for further
-                  encounters.</v-card-text
-                >
-              </v-card>
-            </v-dialog>
             <v-spacer></v-spacer>
             <v-row>
               <v-col>
@@ -144,21 +88,34 @@
                 </p>
               </v-col>
             </v-row>
+            <v-card-actions>
+              <v-btn
+                dark
+                color="blue darken-1"
+                @click.stop="submitRegister"
+                text
+               
+                @click="dialog2 = true"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+            <v-dialog v-model="dialog2" max-width="500px">
+              <v-card>
+                <v-card-title>
+                  <p >Succesfully registered!</p>
+                 
+                </v-card-title>
+                
+                <v-card-actions>
+                  <v-btn color="primary" text @click="dialog2 = false">
+                    Close
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
           </v-container>
         </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-
-          <v-btn
-            color="blue darken-1"
-            @click.stop="submitRegister"
-            text
-            @click="updateD = false"
-          >
-            Save Record
-          </v-btn>
-        </v-card-actions>
       </v-card>
     </v-col>
   </v-row>
@@ -170,6 +127,7 @@ import neo4j from "neo4j-driver";
 export default {
   data: () => ({
     dialog2: false,
+    dialog3: false,
 
     name: null,
     ticker: null,
@@ -181,7 +139,8 @@ export default {
     stockPrice: null,
     director: null,
 
-
+    isOperationSuccess: false,
+    validRegister: "",
   }),
   methods: {
     submitRegister() {
@@ -190,32 +149,38 @@ export default {
         neo4j.auth.basic("neo4j", "DxjItYHh-BDI_QedX3W-Hs2uV8YlP7YZ6pB_ggowcCM")
       );
       var session = driver.session();
-      console.log(this.data);
+      var name = this.name.toLowerCase();
+      var ticker = this.ticker.toUpperCase();
+      var industry = this.industry.toLowerCase();
+      var sector = this.sector.toLowerCase();
+      var director = this.director.toLowerCase();
+
       // // Get a session from the driver
       //   const session = this.$neo4j.getSession();
       session
         .run(
           "MERGE (c:Company {ticker:'" +
-            this.ticker +
+            ticker +
             "', name: '" +
-            this.name +
+            name +
             "'}) MERGE (d:Director {director: '" +
-            this.director +
+            director +
             "'}) MERGE (c)-[:DIRECTED_BY]->(d) MERGE (j:Employee {employee:toInteger(" +
             this.employee +
             ")}) MERGE (c)-[:HAS_EMPLOYEE]->(j) MERGE (f:Industry {industry: '" +
-            this.industry +
+            industry +
             "'}) MERGE (c)-[:INDUSTRALIZE_IN]->(f) MERGE (h:MarketCap {marketCap:toInteger(" +
             this.marketCap +
             ")}) MERGE (c)-[:MARKETCAP_IS]->(h) MERGE (k:Peratio {peRatio:toFloat(" +
             this.peratio +
             ")}) MERGE (c)-[:PERATIO_OF]->(k) MERGE (b:Sector {sector: '" +
-            this.sector +
+            sector +
             "'}) MERGE (c)-[:SECTORED_IN]->(b)  MERGE (m:StockPrice {stockPrice:toFloat(" +
             this.stockPrice +
-            ")}) MERGE (c)-[:STOCKPRICE_OF]->(m) " 
+            ")}) MERGE (c)-[:STOCKPRICE_OF]->(m) "
         )
-        .then(function (result) {
+        .then(function () {
+        
           session.close();
         })
         .catch(function (err) {
@@ -223,8 +188,10 @@ export default {
         });
 
       console.log(this.newArr);
+        document.getElementById("formid").reset();
     },
   },
+
 };
 </script>
  
