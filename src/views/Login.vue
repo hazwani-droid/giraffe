96ea913c7f949266dac5aab3a15d89f00e8e978a
hazwani-droid <hazw200@gmail.com>
2022-06-28 20:33:37 +0800

    login update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-content>
+    <v-main>
       <v-container fluid>
         <v-row align="center" justify="center">
           <v-col cols="12" sm="8" md="8">
@@ -26,30 +26,34 @@
                           </v-btn>
                         </div> -->
                         <!-- <h4 class="text-center mt-4">Ensure your email for registration</h4> -->
-                        <v-form>
+                        <v-form id="login">
                           <v-text-field
-                            label="Email"
+                            label="Username"
                             name="Email"
-                            prepend-icon="mdi-email"
+                            v-model="ruser"
+                            prepend-icon="mdi-account"
                             type="text"
                             color="blue accent-3"
-                            :rules="emailRules"
                           />
 
                           <v-text-field
                             id="password"
                             label="Password"
+                            v-model="rpass"
                             name="password"
                             prepend-icon="mdi-lock"
                             type="password"
                             color="blue accent-3"
-                            :rules="passwordRules"
                           />
                         </v-form>
                         <h3 class="text-center mt-4">Forgot your password ?</h3>
                       </v-card-text>
                       <div class="text-center mt-3">
-                        <v-btn rounded color="blue accent-3" dark
+                        <v-btn
+                          rounded
+                          color="blue accent-3"
+                          dark
+                          @click.stop="login"
                           >SIGN IN</v-btn
                         >
                       </div>
@@ -78,6 +82,13 @@
                           To Keep connected with us please login with your
                           personnel info
                         </h5>
+                        <div
+                          class="card-content m-b-20"
+                          v-for="pigeon in users"
+                          v-bind:key="pigeon.email"
+                        >
+                          <h3>{{ pigeon.email }}</h3>
+                        </div>
                       </v-card-text>
                       <div class="text-center">
                         <v-btn rounded outlined dark @click="step--"
@@ -104,10 +115,11 @@
                           </v-btn>
                         </div>
                         <h4 class="text-center mt-4">Ensure your email for registration</h4> -->
-                        <v-form>
+                        <v-form id="register">
                           <v-text-field
                             label="Username"
                             name="Name"
+                            v-model="user"
                             prepend-icon="mdi-account"
                             type="text"
                             color="blue accent-3"
@@ -116,6 +128,7 @@
                           <v-text-field
                             label="Email"
                             name="Email"
+                            v-model="email"
                             prepend-icon="mdi-email"
                             type="text"
                             color="blue accent-3"
@@ -125,6 +138,7 @@
                           <v-text-field
                             id="password"
                             label="Password"
+                            v-model="pass"
                             name="password"
                             prepend-icon="mdi-lock"
                             type="password"
@@ -134,7 +148,12 @@
                         </v-form>
                       </v-card-text>
                       <div class="text-center mt-n5">
-                        <v-btn rounded color="blue accent-3" dark
+                        <v-btn
+                          rounded
+                          color="blue accent-3"
+                          dark
+                          @click.stop="register"
+                          @click="dialog2 = true"
                           >SIGN UP</v-btn
                         >
                       </div>
@@ -146,14 +165,22 @@
           </v-col>
         </v-row>
       </v-container>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import neo4j from "neo4j-driver";
 export default {
   data: () => ({
     step: 1,
+    ruser: "",
+    rpass: "",
+    user: "",
+    email: "",
+    pass: "",
+    users: [],
+
     stringRules: [
       (v) => !!v || "This field is required",
       (v) => /^[A-Za-z- ]*$/i.test(v) || "Please exclude numbers",
@@ -167,12 +194,89 @@ export default {
     passwordRules: [
       (v) => !!v || "E-mail field is required",
       (v) =>
-        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).*$/.test(v) ||
-        "Password requires 1 of each of the following: uppercase letter, lowercase letter, number, special character",
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).*$/.test(v) ||
+        "Password requires 1 of each of the following: uppercase letter, lowercase letter, number",
     ],
   }),
   props: {
     source: String,
+  },
+  mounted() {
+    this.userload();
+  },
+  methods: {
+    async userload() {
+      var userArr = [];
+      var driver = neo4j.driver(
+        "neo4j://46243086.databases.neo4j.io:7687",
+        neo4j.auth.basic("neo4j", "XIx343iBGlvTvANKgc32XuwQn-n6M1bH-OjhyGV7mlc")
+      );
+      var session = driver.session();
+      //var name = this.name.toLowerCase();
+
+      session
+        .run("MATCH (n:User) RETURN n")
+        .then(function (result) {
+          result.records.forEach(function (record) {
+            userArr.push({
+              username: record._fields[0].properties.username,
+              password: record._fields[0].properties.password,
+            });
+            session.close();
+          });
+        })
+
+        .catch(function (err) {
+          console.log(err, "hhbj");
+        });
+      this.users = userArr;
+    },
+    login() {
+      for (let i = 0; i < this.users.length; i++) {
+        if (
+          this.ruser == this.users[i].username &&
+          this.rpass == this.users[i].password
+        ) {
+          alert("Successfully logged in!");
+          window.location.href = "Home";
+        }
+      }
+      alert("Oops wrong credentials, please try again.");
+    },
+
+    register() {
+      var driver = neo4j.driver(
+        "neo4j://46243086.databases.neo4j.io:7687",
+        neo4j.auth.basic("neo4j", "XIx343iBGlvTvANKgc32XuwQn-n6M1bH-OjhyGV7mlc")
+      );
+      var session = driver.session();
+      var user = this.user;
+      var email = this.email;
+      var pass = this.pass;
+
+      // // Get a session from the driver
+      //   const session = this.$neo4j.getSession();
+      session
+        .run(
+          "MERGE (c:User {username:'" +
+            user +
+            "', email: '" +
+            email +
+            "', password:'" +
+            pass +
+            "'}) "
+        )
+        .then(function () {
+          alert("Successfully registered, please log in to continue.");
+          location.reload();
+          session.close();
+        })
+        .catch(function (err) {
+          console.log(err, "error");
+        });
+
+      document.getElementById("register").reset();
+    },
   },
 };
 </script>
